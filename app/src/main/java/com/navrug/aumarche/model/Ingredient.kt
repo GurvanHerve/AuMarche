9b7fbe50fd571b7f25535770a8ace21f8179aa24
GurvanHERVE <gurvan.herve@gmail.com>
2018-05-13 18:50:21 +1200

    Update model

--- app/src/main/java/com/navrug/aumarche/model/Ingredient.kt
+++ app/src/main/java/com/navrug/aumarche/model/Ingredient.kt
@@ -2,6 +2,4 @@ package com.navrug.aumarche.model
 
 import io.realm.RealmObject
 
-class Ingredient : RealmObject() {
-    var _name: String = ""
-}
+open class Ingredient(var _name: String? = null) : RealmObject()
---
+++ app/src/main/java/com/navrug/aumarche/model/Unit.kt
@@ -0,0 +1,8 @@
+package com.navrug.aumarche.model
+
+enum class Unit {
+    ML,
+    CL,
+    DL,
+    G
+}
