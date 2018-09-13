f48334ee415bc77314fe898682d1b8d24bcfea76
GurvanHERVE <gurvan.herve@gmail.com>
2018-09-13 21:49:43 +1200

    correctly use realm and kotlin

--- app/src/main/java/com/navrug/aumarche/recipes/list/RecipeListPresenter.kt
+++ app/src/main/java/com/navrug/aumarche/recipes/list/RecipeListPresenter.kt
@@ -4,23 +4,42 @@ import com.navrug.aumarche.BasePresenter
 import com.navrug.aumarche.misc.DataGenerator
 import com.navrug.aumarche.model.Recipe
 import io.realm.Realm
+import io.realm.RealmChangeListener
+import io.realm.RealmResults
+import io.realm.kotlin.where
 
-class RecipeListPresenter(private val view: RecipeListView) : BasePresenter<RecipeListView>(view), RecipeAdapter.RecipeListener {
+class RecipeListPresenter(private val view: RecipeListView) : BasePresenter<RecipeListView>(view),
+        RecipeListener, RealmChangeListener<RealmResults<Recipe>> {
     private lateinit var adapter: RecipeAdapter
+    private lateinit var recipes: RealmResults<Recipe>
 
     fun initialize() {
 
         generateData()
 
-        val res = Realm.getDefaultInstance().where(Recipe::class.java).findAll()
+        recipes = Realm.getDefaultInstance().where<Recipe>().findAll()
         adapter = RecipeAdapter(this)
-        adapter.addRecipes(res)
+        adapter.setRecipes(recipes)
         view.setAdapter(adapter)
     }
 
-    override fun onRecipePressed() {
-        view.showRecipe()
+    override fun resume() {
+        super.resume()
+        recipes.addChangeListener(this)
+    }
+
+    override fun pause() {
+        super.pause()
+        recipes.removeChangeListener(this)
+    }
+
+    override fun onRecipePressed(position: Int) {
+        view.showRecipe(recipes[position]!!)
     }
 
     private fun generateData() = DataGenerator().generateRecipes()
+
+    override fun onChange(t: RealmResults<Recipe>) {
+        adapter.notifyDataSetChanged()
+    }
 }
